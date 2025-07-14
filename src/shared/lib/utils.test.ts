2c40b1a4fbff72824355fc9d6ac7aeb549e2df9b
Duvillier Antoine <duvillierantoine@gmail.com>
2025-07-14 11:41:35 +0200

    chore: typo

--- src/shared/lib/utils.test.ts
+++ src/shared/lib/utils.test.ts
@@ -4,9 +4,7 @@ import { describe, expect, it } from 'vitest'
 describe('utils', () => {
   describe('cn', () => {
     it('should merge class names and remove duplicates', () => {
-      if (true) {
-        expect(cn('bg-red-500', 'bg-primary', 'text-white')).toBe('bg-primary text-white')
-      }
+      expect(cn('bg-red-500', 'bg-primary', 'text-white')).toBe('bg-primary text-white')
     })
   })
 })
