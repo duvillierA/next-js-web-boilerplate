753d02a01c97c787bbd7c39b2b8ab11c87050ddd
Duvillier Antoine <duvillierantoine@gmail.com>
2025-08-20 11:48:35 +0200

    chore(ci): implement caching for Playwright browser binaries and adjust installation logic

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -3,7 +3,7 @@ import eslintConfigReactLibrary from '@boilerplate/eslintconfig/react-library'
 export default [
   ...eslintConfigReactLibrary,
   {
-    files: ['src/**/*.tsx'],
+    files: ['src/**/*.{ts,tsx}'],
     rules: {
       'react-refresh/only-export-components': 'off',
     },
