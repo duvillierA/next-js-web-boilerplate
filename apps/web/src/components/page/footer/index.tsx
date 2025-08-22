68ef809f34f9eeaabe49e81c4cd3c22a08912a64
Duvillier Antoine <duvillierantoine@gmail.com>
2025-08-22 12:44:16 +0200

    refactor(footer, header): simplify structure and integrate Stack component for layout

--- apps/web/src/components/page/footer/index.tsx
+++ apps/web/src/components/page/footer/index.tsx
@@ -3,16 +3,12 @@ import { Link } from '@/lib/i18n/navigation'
 
 export const Footer = () => {
   return (
-    <footer className="flex items-center justify-between py-4">
-      <div className="flex flex-row divide-x">
-        <div className="mr-4 flex flex-row items-center space-x-2 pr-4">
-          <h1>
-            <Link href="/">
-              <span className="font-semibold">{config.NEXT_PUBLIC_APP_NAME}</span>
-            </Link>
-          </h1>
-        </div>
-      </div>
+    <footer className="py-4">
+      <p>
+        <Link href="/">
+          <span className="font-semibold">{config.NEXT_PUBLIC_APP_NAME}</span>
+        </Link>
+      </p>
     </footer>
   )
 }
